42b6c65a687a71ed1fbd0811bab7917f42db7670
uzungozilhan <uzungozilhan@yahoo.com>
2021-09-04 15:08:51 +0300

    finished

--- 20210904_reducer/src/App.js
+++ 20210904_reducer/src/App.js
@@ -1,21 +1,26 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+import { reducer } from "./reducer";
+const initialState = {
+  data: "",
+  loading: false,
+  error: "",
+};
 function App() {
-  const [data, setData] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, loading, error } = state;
+  // const [data, setData] = useState("");
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState("");
   const fetchData = () => {
-    setLoading(true);
-    setError("");
-    setData("");
+    dispatch({ type: "FETCH_START" });
     fetch("https://api.thecatapi.com/v1/images/search")
       .then((res) => res.json())
       .then((res) => {
-        setLoading(false);
-        setData(res[0].url)
-      }).catch(()=>{
-        setLoading(false);
-        setError("Something went wrong!")
+        dispatch({ type: "FETCH_SUCCESS", payload: res[0].url });
       })
+      .catch(() => {
+        dispatch({ type: "FETCH_FAIL", payload: "Something went wrong!" });
+      });
   };
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
