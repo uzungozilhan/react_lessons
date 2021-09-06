057722a6ad5a2bac042cb05be02025f5e7fa9526
uzungozilhan <uzungozilhan@yahoo.com>
2021-09-06 06:43:01 +0300

    lesson finished...

--- 20210904_react_styling_2_main/src/App.js
+++ 20210904_react_styling_2_main/src/App.js
@@ -1,12 +1,56 @@
 import "./App.css";
+import Buttons from "./components/material-ui/buttons/Buttons";
+import CheckBoxComp from "./components/material-ui/checkbox/CheckBoxComp";
+import TextFieldComp from "./components/material-ui/text-field/TextFieldComp";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { green, orange } from "@material-ui/core/colors";
+import TypographyComp from "./components/material-ui/typography/TypographyComp";
+import Wrapper from "./components/material-ui/wrapper/Wrapper";
+import Cards from "./components/material-ui/cards/Cards";
+import AppBarComp from "./components/material-ui/app-bar/AppBarComp";
+import StyledButton from "./components/material-ui/styled-button/StyledButton";
+
+// import StyledComponents from "./components/styled-comp";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[400],
+    },
+    secondary: {
+      main: green[300],
+    },
+  },
+
+  typography: {
+    h2: {
+      fontSize: 38,
+      padding: 2,
+    },
+    subtitle1: {
+      marginBottom: 5,
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Styled Components & Material- UI</h1>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <AppBarComp />
+        <div className="App">
+          <header className="App-header">
+            {/* <StyledComponents /> */}
+            <StyledButton />
+            <TypographyComp />
+            <Cards />
+            <TextFieldComp />
+            <CheckBoxComp />
+            <Buttons />
+          </header>
+        </div>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 export default App;
--- 20210904_react_styling_2_main/src/components/material-ui/buttons/Buttons.js
+++ 20210904_react_styling_2_main/src/components/material-ui/buttons/Buttons.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 const Buttons = () => {
   return (
-    <div>
-      <p>Buttons here</p>
-    </div>
+    <ButtonGroup size="large" variant="contained">
+      <Button color="secondary" startIcon={<SaveIcon color="disabled" />}>
+        Save
+      </Button>
+      <Button color="primary" endIcon={<DeleteIcon color="disabled" />}>
+        Delete
+      </Button>
+    </ButtonGroup>
   );
 };
 
