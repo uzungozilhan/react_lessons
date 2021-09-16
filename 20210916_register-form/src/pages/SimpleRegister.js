be54c4285485914df27223ca852b118b49da2891
uzungozilhan <uzungozilhan@yahoo.com>
2021-09-16 21:37:37 +0300

    formik is ok..

--- 20210916_register-form/src/pages/SimpleRegister.js
+++ 20210916_register-form/src/pages/SimpleRegister.js
@@ -1,26 +1,87 @@
-import React from 'react'
-import '../App.css';
+import React from "react";
+import "../App.css";
+import { useFormik } from "formik";
 
 function SimpleRegister() {
-    return (
-        <div className="container">
-            <h1>Simple Form</h1>
-            
-            <form className="formStyle">
-                <label  htmlFor="name">Name</label>
-                <input type="text" id="name" name="name"></input>
-                
-                <label  htmlFor="email">Email</label>
-                <input type="text" id="email" name="email"></input>
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      userName: "",
+    },
+    onSubmit: (values) => {
+      console.log("Values:", values);
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.name) {
+        errors.name = "Required. Please fill the area";
+      }
+      if (!values.email) {
+        errors.email = "Required. Please fill the area";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+      if (!values.userName) {
+        errors.userName = "Required. Please fill the area";
+      }
 
-                <label htmlFor="userName">User name</label>
-                <input type="text" id="userName" name="userName"></input>
+      return errors;
+    },
+  });
 
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+//   console.log("Formik values:", formik.values);
+//   console.log("Formik :", formik);
+    console.log("formik touch:", formik.touch)
+
+  return (
+    <div className="container">
+      <h1>Simple Form</h1>
+
+      <form className="formStyle" onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+                  value={formik.values.name}
+                  onBlur={formik.handleBlur}
+        ></input>
+        {formik.touched.name && formik.errors.name ? (
+          <div className="errorStyle">{formik.errors.name}</div>
+        ) : null}
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        ></input>
+        {formik.touched.email && formik.errors.email ? (
+          <div className="errorStyle">{formik.errors.email}</div>
+        ) : null}
+
+        <label htmlFor="userName">User name</label>
+        <input
+          type="text"
+          id="userName"
+          name="userName"
+          onChange={formik.handleChange}
+          value={formik.values.userName}
+        ></input>
+        {formik.touched.userName && formik.errors.userName ? (
+          <div className="errorStyle">{formik.errors.userName}</div>
+        ) : null}
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default SimpleRegister
-
+export default SimpleRegister;
