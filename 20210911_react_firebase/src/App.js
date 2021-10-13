9c3def5218f8c2997c3fbdbacb56f49d6a112613
uzungozilhan <uzungozilhan@yahoo.com>
2021-10-13 21:53:22 +0300

    firebase'e geçiş

--- 20210911_react_firebase/src/App.js
+++ 20210911_react_firebase/src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import FormComponent from '../src/components/form/Form';
+import Contacts from '../src/components/contacts/Contacts'
+
+const initialState = { username: "", phoneNumber: "", gender: "NO INFO" };
+
+
 function App() {
-  return <div className='App'>firecontact</div>
+
+  
+  const [info, setInfo] = useState(initialState);
+
+  return <div className='App'>
+    <FormComponent
+        
+        className="form"
+        info={info}
+        setInfo={setInfo}
+      />
+    <Contacts/>
+  </div>
 }
 
 export default App;
